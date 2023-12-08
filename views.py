1813a94d78dad311084b574883bd57cbff54b3f0
damienwin <134009438+damienwin@users.noreply.github.com>
2023-12-08 00:39:58 -0600

    Navbar and Pages
    Added navigatable pages and sued templates to keep formatting

--- views.py
+++ views.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template, redirect, url_for, request, jsonify
 
-views = Blueprint(__name__, "views")
+views = Blueprint(__name__, 'views')
 
 @views.route("/")
 def home():
     return render_template("index.html")
+
+@views.route("/about")
+def about():
+    return render_template("about.html")
+
+@views.route("/repo")
+def repo():
+    return render_template("repo.html")
+
