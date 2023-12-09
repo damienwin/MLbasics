7a05f2f15bad5639d5290b9b7d6a540c6cd40e35
damienwin <134009438+damienwin@users.noreply.github.com>
2023-12-09 15:22:48 -0600

    Views Rerouting
    Added previous and next page buttons for categories, reroutes to correct url, but still a bug in not loading a different html.

--- views.py
+++ views.py
@@ -2,15 +2,32 @@ from flask import Blueprint, render_template, redirect, url_for, request, jsonif
 
 views = Blueprint(__name__, 'views')
 
-@views.route("/")
+sections = {
+    'introduction': ['introduction_page', 'introduction_page2', 'introduction_page3']
+}
+
+@views.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@views.route("/about")
+@views.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
 
-@views.route("/repo")
+@views.route('/repo')
 def repo():
-    return render_template("repo.html")
+    return render_template('repo.html')
 
+@views.route('/<category>/<page>')
+def show_page(category, page):
+    print(f"Category: {category}, Page: {page}")
+    if category in sections:
+        current_section = sections[category]
+        print(f"Current Section: {current_section}")
+        if page in current_section:
+            current_index = current_section.index(page)
+            prev_page = current_section[current_index - 1] if current_index > 0 else None
+            next_page = current_section[current_index + 1] if current_index < len(current_section) - 1 else None
+            print(f"Prev Page: {prev_page}, Next Page: {next_page}")
+            return render_template(f'{category}/{category}_page.html', category=category, page=page, prev_page=prev_page, next_page=next_page)
+    return "Page not found", 404
