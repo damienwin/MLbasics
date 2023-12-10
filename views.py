8cd50087c0b2e2acb127b16b9baf3492cbc8c941
damienwin <134009438+damienwin@users.noreply.github.com>
2023-12-10 13:42:50 -0600

    Working Dynamic Rerouting
    Fixed html file not loading problem

--- views.py
+++ views.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, jsonif
 views = Blueprint(__name__, 'views')
 
 sections = {
-    'introduction': ['introduction_page', 'introduction_page2', 'introduction_page3']
+    'introduction': ['page1', 'page2', 'page3']
 }
 
 @views.route('/')
@@ -20,14 +20,11 @@ def repo():
 
 @views.route('/<category>/<page>')
 def show_page(category, page):
-    print(f"Category: {category}, Page: {page}")
     if category in sections:
         current_section = sections[category]
-        print(f"Current Section: {current_section}")
         if page in current_section:
             current_index = current_section.index(page)
             prev_page = current_section[current_index - 1] if current_index > 0 else None
             next_page = current_section[current_index + 1] if current_index < len(current_section) - 1 else None
-            print(f"Prev Page: {prev_page}, Next Page: {next_page}")
-            return render_template(f'{category}/{category}_page.html', category=category, page=page, prev_page=prev_page, next_page=next_page)
+            return render_template(f'{category}/{page}.html', category=category, page=page, prev_page=prev_page, next_page=next_page)
     return "Page not found", 404
