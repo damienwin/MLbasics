05ec16b3290669ee519762c0c43b3d1406b8e8be
damienwin <134009438+damienwin@users.noreply.github.com>
2023-12-27 21:28:58 -0600

    Linear Graph Page 2 Function
    Added linear graph with a button that adds a new randomly generated point on the line.

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import Flask, request, send_from_directory, render_template
+from flask import Flask, request, send_from_directory, render_template, jsonify, render_template_string
 import plotly.express as px
 import numpy as np
-
-
 
 app = Flask(__name__)
 
@@ -13,9 +11,10 @@ sections = {
 def get_result():
     # make data
     x = np.linspace(0, 10, 100)
+    y = .5 * x + 2
     
     # graph lines and points on figure
-    fig = px.line(x=x, y=(0.5 * x + 2), labels={'x': 'x-axis', 'y': 'y-axis'})
+    fig = px.line(x=x, y=y, labels={'x': 'x-axis', 'y': 'y-axis'})
     fig.update_traces(showlegend=True, name ='w*x + b')
     fig.add_scatter(x=[0], y=[2], mode='markers', marker=dict(color='red', size=10), name='y-intercept (b)')
 
@@ -66,6 +65,55 @@ def linearpage():
     next_page = sections[category][sections[category].index(page) + 1] if page in sections[category] and sections[category].index(page) < len(sections[category]) - 1 else None
 
     return render_template('linear_regression/page1.html', html_result=python_result, prev_page=prev_page, next_page=next_page)
+
+initial_x = np.linspace(1, 8, 16)
+initial_y = 25 * initial_x + np.random.randn(16) * 16 + 100
+
+def initial_graph():
+    fig = px.scatter(x=initial_x, y=initial_y, labels={'x': 'sq. feet in thousands', 'y': '$ price in thousands'})
+    fig.add_scatter(x=initial_x, y=(25 * initial_x + 100), mode='lines', name="Prediction Line: (y = 25x + 100)")
+
+    fig.update_layout(
+        title="Pricing vs. Square Feet of Houses",
+        template='simple_white',
+        xaxis=dict(range=[1, 8], dtick=1, showgrid=True),
+        yaxis=dict(range=[100, 300], dtick=50, showgrid=True), 
+        width=700, height=400
+    )
+
+    return fig
+
+@app.route('/linear_regression/page2')
+def linearpage2():
+    res = initial_graph().to_html(full_html=False)
+    
+    category = 'linear_regression'
+    page = 'page2'
+    prev_page = sections[category][sections[category].index(page) - 1] if page in sections[category] and sections[category].index(page) > 0 else None
+    next_page = sections[category][sections[category].index(page) + 1] if page in sections[category] and sections[category].index(page) < len(sections[category]) - 1 else None
+
+    return render_template('linear_regression/page2.html', initial_graph=res, prev_page=prev_page, next_page=next_page)
+
+
+@app.route('/update-plot', methods=['GET'])
+def update_plot():
+    randx = np.random.uniform(1, 8)
+    randy = 25 * randx + 100  # Use the prediction line equation
+
+    # Create or update the Plotly plot using the accumulated data
+    fig = initial_graph()
+    fig.add_scatter(
+        x=[randx], 
+        y=[randy], 
+        mode='markers', 
+        marker=dict(color='red', size=10), 
+        name=f'Predicted Data Point:<br>{int(randx*1000)} ft.<sup>2</sup><br>${int(randy) * 1000}'
+    )
+
+    # Convert the updated Plotly figure to HTML and return it to the HTML page
+    plot_html = fig.to_html(full_html=False)
+    return render_template('linear_regression/page2.html', initial_graph=plot_html, prev_page='page1', next_page='page3')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
